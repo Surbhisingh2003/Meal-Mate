6c08719781451678205edb7d1211b133dcc4a647
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-05-17 22:30:24 +0530

    Day17May

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='header'>
             <div className='header-contents'>
-                <h2>Order your favourite food here</h2>
+                <marque><h2>Order your favourite food here</h2></marque>
                 <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
                 <button>View Menu</button>
             </div>
