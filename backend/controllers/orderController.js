d289de4474f30f6508f8153bd6d574bd7be23484
Aditya Prakash Pandey <adityapandey03012003@gmail.com>
2024-06-06 10:15:50 +0530

    Initial commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -21,7 +21,7 @@ const placeOrder = async (req, res) => {
               product_data: {
                 name: item.name
               },
-              unit_amount: item.price*100*80
+              unit_amount: item.price*100
             },
             quantity: item.quantity
           }))
@@ -32,7 +32,7 @@ const placeOrder = async (req, res) => {
                 product_data:{
                     name:"Delivery Charge"
                 },
-                unit_amount: 5*80*100
+                unit_amount: 5*100
             },
             quantity:1
         })
